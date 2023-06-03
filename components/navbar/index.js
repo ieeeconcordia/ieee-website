63a7a39b519fa00f5e8e77db6bdc43cdcac2fb41
Shamiivan <shamiivan@gmail.com>
2023-06-03 12:39:57 -0400

    Add routes to every components

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,10 +1,11 @@
-import Container from "react-bootstrap/Container";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
+    <nav className="nav_container">
     <div
       style={{ flexDirection: "row", display: "flex", alignItems: "center", justifyContent: 'space-between' }}
-    >
+      >
       <h3>IEEE Concordia</h3>
       <h4>Lab status: Open</h4>
       <div style={{display: 'flex' ,flexDirection: 'row', gap: 5}}>
@@ -16,14 +17,43 @@ export default function Navbar() {
             margin: 0,
           }}
         >
-          <li style={{ display: "inline" }}>Link</li>
-          <li style={{ display: "inline" }}>Link</li>
-          <li style={{ display: "inline" }}>Link</li>
-          <li style={{ display: "inline" }}>Link</li>
-          <li style={{ display: "inline" }}>Link</li>
+          <li style={{ display: "inline" }}>
+          <Link href="/aboutUs">
+             aboutUs
+            </Link>
+          </li>
+
+
+          <li style={{ display: "inline" }}>
+          <Link href="/events">
+              events
+            </Link>
+          </li>
+
+
+          <li style={{ display: "inline" }}>
+          <Link href="/projects">
+              projects
+            </Link>
+          </li>
+
+
+          <li style={{ display: "inline" }}>
+          <Link href="/laboratory">
+              lab
+            </Link>
+          </li>
+
+          <li style={{ display: "inline" }}>
+
+          <Link href="contact">
+              contact us
+            </Link>
+          </li>
         </ul>
         <button type="button">Sponsor</button>
       </div>
     </div>
+    </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/navbar";
-import AboutUs from "../components/sections/AboutUs";
 import Events from "../components/sections/Events"
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
 
   return (
     <>
+      <Navbar />
+       
       <Events />
     </>
   )
