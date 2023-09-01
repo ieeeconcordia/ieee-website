2acc14b8323f0903ae7c66d22c7027357ba71733
Webmaster-ieeeConcordia <114526522+Webmaster-ieeeConcordia@users.noreply.github.com>
2023-08-31 23:22:28 -0400

    fixed typescript issue

--- components/buttons/SponsorshipBtn.tsx
+++ components/buttons/SponsorshipBtn.tsx
@@ -2,16 +2,17 @@ import sponsorship from "@/pages/sponsorship";
 import Link from "next/link";
 import React from "react";
 
-export const SponsorshipBtn = ({ link, text }) => {
+interface SponsorshipBtnProps {
+  link: string;
+  text: string;
+}
+
+export const SponsorshipBtn = ({ link, text }: SponsorshipBtnProps) => {
   return (
     <Link href={link}>
-      <div
-        className={
-          " w-fit py-4 px-6 bg-gradient-to-r from-sky-400 via-cyan-400 to-sky-400 text-white font-raleway font-semibold text-title-m rounded-md shadow-md"
-        }
-      >
+      <a className="inline-block py-4 px-6 bg-gradient-to-r from-sky-400 via-cyan-400 to-sky-400 text-white font-raleway font-semibold text-title-m rounded-md shadow-md">
         {text}
-      </div>
+      </a>
     </Link>
   );
 };
