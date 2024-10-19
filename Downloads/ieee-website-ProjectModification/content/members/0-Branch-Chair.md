bcd7a9dcab10be980d7f04a85c8130c7c804d1d9
Webmaster-ieeeConcordia <webmaster@ieeeconcordia.ca>
2024-10-19 16:14:25 -0400

    ok

---
+++ Downloads/ieee-website-ProjectModification/content/members/0-Branch-Chair.md
@@ -0,0 +1,10 @@
+---
+image: /team24-25/Augusto.webp
+role: Branch Chair
+name: Augusto Mota Pinheiro
+email: chair@ieeeconcordia.ca
+program: COMP
+linkedin: 'https://www.linkedin.com/in/augustomp'
+github: 'https://github.com/AugustDG'
+---
+
---
+++ Downloads/ieee-website-ProjectModification/pages/laboratory.tsx
@@ -0,0 +1,38 @@
+// components/ImageUploader.js
+import React, { useState } from "react";
+import cloudinary from "@/lib/cloudinary";
+
+function ImageUploader() {
+  const [image, setImage] = useState(null);
+
+  const handleImageUpload = async (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "klyl1arw");
+
+      const response = await fetch(
+        "https://api.cloudinary.com/v1_1/ddds30vut/upload",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      const data = await response.json();
+      setImage(data.secure_url);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageUpload} />
+      {image && <img src={image} alt="Uploaded" />}
+      <button onClick={handleImageUpload}> Submit </button>
+    </div>
+  );
+}
+
+export default ImageUploader;
