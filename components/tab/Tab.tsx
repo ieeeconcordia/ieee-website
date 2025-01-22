6909d9f86fd451c67445f1d6c655d1c6a10f13bf
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2025-01-21 22:43:12 -0500

    I fixed a couple of issues based on the feedback I received in the lab and made it more responsive for mobile and tablet versions

--- components/tab/Tab.tsx
+++ components/tab/Tab.tsx
@@ -1,25 +1,61 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Member } from "@/components/cards/Member";
 
   export const Tab = ({ members }: any) => {
       const tabs = ['Chair', 'Academics', 'IT', 'Competitions', 'Development', 'External', 'Internal', 'Finance', 'Community', 'Lab Services', 'Projects', 'Marketing', 'Notion', 'Advisor'];
       const [activeTab, setActiveTab] = useState(tabs[0]);
       const [startIndex, setStartIndex] = useState(0);
+      const [tabsToShow, setTabsToShow] = useState(3);
+
+      useEffect(() => {
+        const updateTabsToShow = () => {
+          if (window.innerWidth >= 1024) {
+            setTabsToShow(7);
+          } else if (window.innerWidth >= 768) {
+            setTabsToShow(5);
+          } else {
+            setTabsToShow(2);
+          }
+        };
+        updateTabsToShow();
+        window.addEventListener('resize', updateTabsToShow);
     
+        return () => {
+          window.removeEventListener('resize', updateTabsToShow);
+        };
+      }, []);
+
+      
       const handleTabClick = (tab: string) => {
         setActiveTab(tab);
       };
     
       const handleNext = () => {
-        if (startIndex + 5 < tabs.length) {
-          setStartIndex(startIndex + 5);
-        }
+        if (window.innerWidth >= 1024) {
+          if (startIndex + tabsToShow < tabs.length) {
+            setStartIndex(startIndex + 7);
+          }
+        } else if (window.innerWidth >= 768) {
+          if (startIndex + tabsToShow < tabs.length) {
+            setStartIndex(startIndex + 5);
+          }
+        } else if (startIndex + tabsToShow < tabs.length) {
+            setStartIndex(startIndex + 2 );
+          }
       };
     
       const handlePrev = () => {
-        if (startIndex - 5 >= 0) {
-          setStartIndex(startIndex - 5);
-        }
+        if (window.innerWidth >= 1024) {
+          if (startIndex >= 0) {
+            setStartIndex(startIndex - 7);
+          }
+        } else if (window.innerWidth >= 768) {
+          if (startIndex >= 0) {
+            setStartIndex(startIndex - 5);
+          }
+        } else if (startIndex >= 0) {
+            setStartIndex(startIndex - 2);
+          }
       };
 
       
@@ -61,34 +97,31 @@ import { Member } from "@/components/cards/Member";
       }
     };
 
-
     return (
       <div>
-        <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 mb-4">
+        <div className="text-base font-medium text-center border-b border-blue-200 dark:text-blue-400 dark:border-blue-700 mb-4">
           <div className="flex justify-between items-center">
-            <button onClick={handlePrev} disabled={startIndex === 0} className="p-2">
-              &lt;
+            <button onClick={handlePrev} disabled={startIndex === 0} className="p-2 text-xl hover:text-blue-600 hover:border-blue-300 dark:hover:text-blue-300">
+              &lt;-
             </button>
-            <ul className="flex overflow-hidden">
-              {tabs.slice(startIndex, startIndex + 5).map((tab) => (
-                <li className="me-2" key={tab}>
-                  <a
-                    href="#"
+            <ul className="flex overflow-hidden text-gray-500">
+              {tabs.slice(startIndex, startIndex + tabsToShow).map((tab, index) => (
+                <li className="me-2" key={index}>
+                  <button
                     className={`inline-block p-4 border-b-2 rounded-t-lg ${
                       activeTab === tab
                         ? "text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500"
-                        : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                        : "border-transparent hover:text-blue-600 hover:border-blue-300 dark:hover:text-blue-300"
                     }`}
                     onClick={() => handleTabClick(tab)}
-                    aria-current={activeTab === tab ? "page" : undefined}
                   >
                     {tab}
-                  </a>
+                  </button>
                 </li>
               ))}
             </ul>
-            <button onClick={handleNext} disabled={startIndex + 5 >= tabs.length} className="p-2">
-              &gt;
+            <button onClick={handleNext} disabled={startIndex + tabsToShow >= tabs.length} className="p-2 text-xl hover:text-blue-600 hover:border-blue-300 dark:hover:text-blue-300">
+              -&gt;
             </button>
           </div>
         </div>
@@ -99,4 +132,43 @@ import { Member } from "@/components/cards/Member";
         </div>
       </div>
     );
-  };
+  }      
+
+//     return (
+//       <div>
+//         <div className="text-base font-medium text-center border-b border-blue-200 dark:text-blue-400 dark:border-blue-700 mb-4">
+//           <div className="flex justify-between items-center">
+//             <button onClick={handlePrev} disabled={startIndex === 0} className="p-2 text-xl hover:text-blue-600 hover:border-blue-300 dark:hover:text-blue-300">
+//               &lt;
+//             </button>
+//             <ul className="flex overflow-hidden text-gray-500  ">
+//               {tabs.slice(startIndex, startIndex + 5).map((tab) => (
+//                 <li className="me-2" key={tab}>
+//                   <a
+//                     href="#"
+//                     className={`inline-block p-4 border-b-2 rounded-t-lg ${
+//                       activeTab === tab
+//                         ? "text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500"
+//                         : "border-transparent hover:text-blue-600 hover:border-blue-300 dark:hover:text-blue-300"
+//                     }`}
+//                     onClick={() => handleTabClick(tab)}
+//                     aria-current={activeTab === tab ? "page" : undefined}
+//                   >
+//                     {tab}
+//                   </a>
+//                 </li>
+//               ))}
+//             </ul>
+//             <button onClick={handleNext} disabled={startIndex + 5 >= tabs.length} className="p-2  text-xl hover:text-gray-600 hover:border-blue-300 dark:hover:text-blue-300">
+//               &gt;
+//             </button>
+//           </div>
+//         </div>
+//         <div className="w-fit grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4 sm:gap-10">
+//           {getMembersForTab(activeTab).map((member: any) => (
+//             <Member key={member.id} {...member} />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   };
